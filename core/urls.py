da2ef231273a5872f3e5e6edba0f84abdad24352
talex <talex.kaliningrad.com>
2025-08-23 20:57:06 +0200

    Add search and bad eye

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomePageView, AchievementListView
+from .views import HomePageView, AchievementListView, search_results
 
 app_name = 'core'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('achievements/', AchievementListView.as_view(), name='achievement_list'),
+    path('search/', search_results, name='search_results'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import TemplateView, ListView
 from .models import Achievement
+from django.shortcuts import render
+from django.db.models import Q
+from news.models import NewsArticle
 
 class HomePageView(TemplateView):
     template_name = "home.html"
@@ -8,3 +11,18 @@ class AchievementListView(ListView):
     model = Achievement
     template_name = 'core/achievement_list.html'
     context_object_name = 'achievements'
+
+def search_results(request):
+    query = request.GET.get('q')
+    results = []
+
+    if query:
+        results = NewsArticle.objects.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query)
+        )
+
+    return render(request, 'search_results.html', {
+        'query': query,
+        'results': results
+    })
